7d3d90c275b66596765f2fd80f64624fc947e921
Дмитрий Петинов <dpetinov@ibs.ru>
2024-04-16 19:40:03 +0400

    JavaBasic Task 2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        double dnum = 0.8D; // переменная типа с плавающей точкой с наибольшим диапазоном чисел (double)
+        byte bnum = 127; //переменная целочисленного типа с наименьшим диапазоном чисел (byte) с наибольшим возможным значением
+        System.out.println("dnum = " + dnum + " // переменная типа с плавающей точкой с наибольшим диапазоном чисел (double)");
+        System.out.println("bnum = " + bnum + " //переменная целочисленного типа с наименьшим диапазоном чисел (byte) с наибольшим возможным значением");
+
+    }
+}
